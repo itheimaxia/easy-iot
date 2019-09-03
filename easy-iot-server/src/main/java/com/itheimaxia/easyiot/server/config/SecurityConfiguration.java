9a5c470bdb84c012beede35175672b7f2e2c718d
itheimaxia <xiadong@itcast.cn>
2019-09-03 23:17:33 +0800

    SS框架整合

---
+++ easy-iot-server/src/main/java/com/itheimaxia/easyiot/server/config/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.itheimaxia.easyiot.server.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebSecurity
+public class SecurityConfiguration implements WebMvcConfigurer {
+    @Bean
+    public UserDetailsService userDetailsService() throws Exception {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build());
+        return manager;
+    }
+
+}
