b217ee3f9af81680d2b048ee34d4bc0dd529b19e
itheimaxia <xiadong@itcast.cn>
2019-09-05 22:10:04 +0800

    添加登录页面

--- easy-iot-server/src/main/java/com/itheimaxia/easyiot/server/config/SecurityConfiguration.java
+++ easy-iot-server/src/main/java/com/itheimaxia/easyiot/server/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,12 +13,25 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebSecurity
-public class SecurityConfiguration implements WebMvcConfigurer {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
-    public UserDetailsService userDetailsService() throws Exception {
+    public UserDetailsService userDetailsService() {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
         manager.createUser(User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build());
         return manager;
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+        //http.headers().frameOptions().sameOrigin();
+        http
+                .authorizeRequests()
+                .antMatchers("/css/**", "/img/**", "/js/**","/plugins/**","/pages/login.html").permitAll()
+
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/pages/login.html")
+                .permitAll();
     }
 
 }
